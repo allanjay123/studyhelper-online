fe5be99e9a6dc3b14a790bd126427e38e14dcc65
allanjay123 <allantagapan74@gmail.com>
2026-01-17 07:19:27 +0800

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,15 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCk3YyAne_nz0fwChI6557R8Rtcm1MAawA",
+  authDomain: "studyhelper-app.firebaseapp.com",
+  projectId: "studyhelper-app",
+  storageBucket: "studyhelper-app.appspot.com",
+  messagingSenderId: "390954392930",
+  appId: "1:390954392930:web:4c7ebc6a37112e98dce03b"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);

becff27b972919c5007d62d307c4ded559a84a7b
allanjay123 <allantagapan74@gmail.com>
2026-01-17 07:37:41 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,17 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCk3YyAne_nz0fwChI6557R8RtcmlMAawA",
+  apiKey: "AIzaSyCk3YyAne_nz0fwChI6557R8Rtcm1MAawA",
   authDomain: "studyhelper-app.firebaseapp.com",
   projectId: "studyhelper-app",
-  storageBucket: "studyhelper-app.firebasestorage.app",
+  storageBucket: "studyhelper-app.appspot.com",
   messagingSenderId: "390954392930",
   appId: "1:390954392930:web:4c7ebc6a37112e98dce03b"
 };
-export { firebaseConfig };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };

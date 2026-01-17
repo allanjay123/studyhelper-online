c8b234c780c4e56c92390447f1de907c3d9bdb1b
allanjay123 <allantagapan74@gmail.com>
2026-01-17 09:41:46 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCk3YyAne_nz0fwChI6557R8RtcmlMAawA",
   authDomain: "studyhelper-app.firebaseapp.com",
   projectId: "studyhelper-app",
-  storageBucket: "studyhelper-app.firebasestorage.app",
+  storageBucket: "studyhelper-app.appspot.com", // ✅ IMPORTANT
   messagingSenderId: "390954392930",
   appId: "1:390954392930:web:4c7ebc6a37112e98dce03b"
 };
